be1a50b9927bb796646cf92b6f3953225ef5c31a
Neathan Kungu <natekungu857@gmail.com>
2025-11-24 11:16:43 +0300

    Indentation errors resolved

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,9 +2,9 @@ class Article:
     all = []
     
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        self._author = author
+        self._magazine = magazine
+        self._title = title
         Article.all.append(self)
         
     @property
@@ -13,26 +13,21 @@ class Article:
 
     @title.setter
     def title(self, title):
-        if(
+        if (
             isinstance(title, str)
-            and 5 <= len(title) <=50
-            and not hasatrr(self, "title")
+            and 5 <= len(title) <= 50
+            and not hasattr(self, "_title")
         ):
-
-        self._title = title
-        else:
-            return None
+            self._title = title
 
     @property
     def author(self):
-        return self.author
+        return self._author
 
     @author.setter
     def author(self, author):
-        if isinstance(author, Author)
-         self._author = author
-        else:
-            return None
+        if isinstance(author, Author):
+            self._author = author
 
     @property
     def magazine(self):
@@ -41,9 +36,7 @@ class Article:
     @magazine.setter
     def magazine(self, magazine):
         if isinstance(magazine, Magazine):
-            self._magazine = magazine
-        else:
-            return None 
+            self._magazine = magazine 
 
 class Author:
     def __init__(self, name):
